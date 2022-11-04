51bbdeb0e320fa3851e9dbd9546714245baf1534
josuebuenor <josuebuenor@ufrrj.br>
2022-11-03 23:50:54 -0300

    Aula 02

---
+++ Aula02/aula02.py
@@ -0,0 +1,15 @@
+#num = 1
+#num = "Jo"
+#num = 1.6 
+
+canal = "Curso" ; curso = "de Python"
+
+print(canal + " " + curso) # Concatenando variaveis 
+
+zero = n1 = a = 0 # Colocando o mesmo valor para várias variaveis 
+
+# Mostrando como funciona os escopos
+if 10 > 2:
+    print(" Maior")
+    print(" Dentro do If")
+print("Fora do If")
